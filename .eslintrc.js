6bb2ccb050a6e8d63fe9ac588a79874896fc193a
Victor Reinor <victorreinor@gmail.com>
2019-08-03 11:48:24 -0300

    adiciona eslint com suporte para o react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -30,6 +31,10 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js'] }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
 };
